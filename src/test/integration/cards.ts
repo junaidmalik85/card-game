3f57a7ee89125f9e7294a30d83165fd095d56f04
Muhammad Junaid Malik <junaidmalik85@users.noreply.github.com>
2021-11-22 06:03:30 +0500

    more test added

--- src/test/integration/cards.ts
+++ src/test/integration/cards.ts
@@ -1,11 +1,14 @@
 import { expect } from 'chai'
 import request from 'supertest';
 import { cards } from '../mocks/unshafffledFullCards'
+import { cardsShort } from '../mocks/unshaffledShortCards'
 
 describe("Cards APIs", () => {
     let server;
     let shuffledDeckId: string;
     let unshuffledDeckId: string;
+    let shuffledShortDeckId: string;
+    let unshuffledShortDeckId: string;
     before(async () => {
         const mod = await import('../../index');
         server = (mod as any).default;
@@ -16,7 +19,7 @@ describe("Cards APIs", () => {
         }
     });
 
-    it('POST /cards/create - Create a deck with full 52 cards (non shuffled)', async () => {
+    it('POST /cards/create - Create a deck with full 52 cards (unshuffled)', async () => {
         const res = await request(server)
             .post('/cards/create')
             .set('Accept', 'application/json')
@@ -32,7 +35,7 @@ describe("Cards APIs", () => {
         expect(res.body.remaining).to.equal(52);
     })
 
-    it('get /cards/open/:deckId - Get a deck with full 52 cards (non shuffled)', async () => {
+    it('get /cards/open/:deckId - Get a deck with full 52 cards (unshuffled)', async () => {
         const res = await request(server)
             .get('/cards/open/' + unshuffledDeckId)
             .set('Accept', 'application/json')
@@ -61,7 +64,6 @@ describe("Cards APIs", () => {
         expect(res.body.remaining).to.equal(52);
     })
 
-    
     it('get /cards/open/:deckId - Get a deck with full 52 cards (shuffled)', async () => {
         const res = await request(server)
             .get('/cards/open/' + shuffledDeckId)
@@ -75,7 +77,7 @@ describe("Cards APIs", () => {
         expect(res.body.remaining).to.equal(52);
     })
 
-    it('POST /cards/create - Create a deck with short 36 cards', async () => {
+    it('POST /cards/create - Create a deck with short 36 cards (unshuffled)', async () => {
         const res = await request(server)
             .post('/cards/create')
             .set('Accept', 'application/json')
@@ -83,8 +85,52 @@ describe("Cards APIs", () => {
                 "type": "SHORT",
                 "shuffled": false
             });
+        unshuffledShortDeckId = res.body.deckId;
         expect(res.status).to.equal(200);
         expect(res.body.deckId).to.be.a("string");
+        expect(res.body.type).to.equal("SHORT");
+        expect(res.body.shuffled).to.equal(false);
+        expect(res.body.remaining).to.equal(36);
+    })
+
+    it('get /cards/open/:deckId - Get a deck with short 36 cards (unshuffled)', async () => {
+        const res = await request(server)
+            .get('/cards/open/' + unshuffledShortDeckId)
+            .set('Accept', 'application/json')
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body.deckId).to.be.a("string");
+        expect(res.body.shuffled).to.equal(false);
+        expect(res.body.cards).to.deep.equal(cardsShort);
+        expect(res.body.type).to.equal("SHORT");
+        expect(res.body.remaining).to.equal(36);
+    })
+
+    it('POST /cards/create - Create a deck with short 36 cards (shuffled)', async () => {
+        const res = await request(server)
+            .post('/cards/create')
+            .set('Accept', 'application/json')
+            .send({
+                "type": "SHORT",
+                "shuffled": true
+            });
+        shuffledShortDeckId = res.body.deckId;
+        expect(res.status).to.equal(200);
+        expect(res.body.deckId).to.be.a("string");
+        expect(res.body.type).to.equal("SHORT");
+        expect(res.body.shuffled).to.equal(true);
+        expect(res.body.remaining).to.equal(36);
+    })
+
+    it('get /cards/open/:deckId - Get a deck with short 36 cards (shuffled)', async () => {
+        const res = await request(server)
+            .get('/cards/open/' + shuffledShortDeckId)
+            .set('Accept', 'application/json')
+            .send();
+        expect(res.status).to.equal(200);
+        expect(res.body.deckId).to.be.a("string");
+        expect(res.body.shuffled).to.equal(true);
+        expect(res.body.cards).to.not.equal(cardsShort);
         expect(res.body.type).to.equal("SHORT");
         expect(res.body.remaining).to.equal(36);
     })
